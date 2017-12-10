6be8d67c3baad7ff4231fe1fa91e7bd2c5a2d102
reza <ryarbakhsh@gmail.com>
2017-12-10 18:55:45 +0330

    physics debugged

--- src/physics.c
+++ src/physics.c
@@ -18,16 +18,16 @@ Direction decideGhost(const Map* map, Ghost* ghost) {
         dir = rand() % 5;
         switch (dir) {
             case 1:
-                destin_y = (int)round(ghost->y - 1);
+                destin_y = (int)ghost->y - 1;
                 break;
             case 2:
-                destin_x = (int)round(ghost->x + 1);
+                destin_x = (int)ghost->x + 1;
                 break;
             case 3:
-                destin_y = (int)round(ghost->y + 1);
+                destin_y = (int)ghost->y + 1;
                 break;
             case 4:
-                destin_x = (int)round(ghost->x - 1);
+                destin_x = (int)ghost->x - 1;
                 break;
         }
         destin_x = destin_x < 0 ? destin_x % map->width + map->width : destin_x % map->width;
@@ -39,52 +39,51 @@ Direction decideGhost(const Map* map, Ghost* ghost) {
 }
 
 Direction decidePacman(const Map* map, Pacman* pacman, Action action) {
-/*    int dir = pacman->dir;
+    int dir = pacman->dir;
     int destin_y = pacman->y, destin_x =pacman->x;
-    switch (action){
-        case ACTION_UP:
-            destin_y = (int)pacman->y - 1;
-            dir=1;
-            break;
-        case ACTION_RIGHT:
-            destin_x = (int)pacman->x + 1;
-            dir=2;
-            break;
-        case ACTION_DOWN:
-            destin_y = (int)pacman->y + 1;
-            dir=3;
-            break;
-        case ACTION_LEFT:
-            destin_x = (int)pacman->x - 1;
-            dir=4;
-            break;
-    }
-    destin_x = destin_x < 0 ? destin_x % map->width + map->width : destin_x % map->width;
-    destin_y = destin_y < 0 ? destin_y % map->height + map->height : destin_y % map->height;
-    if (map->cells[destin_x][destin_y] != CELL_BLOCK) return dir;
-    else{
-        switch (pacman->dir){
-            case 1:
-                destin_y = (int)pacman->y - 1;
+    printf("\n%d %d\n",action,MOVE_ACTION_MASK);
+    if (action & MOVE_ACTION_MASK) {
+        switch (action) {
+            case ACTION_UP:
+                destin_y = (int) pacman->y - 1;
+                dir = 1;
                 break;
-            case 2:
-                destin_x = (int)pacman->x + 1;
+            case ACTION_RIGHT:
+                destin_x = (int) pacman->x + 1;
+                dir = 2;
                 break;
-            case 3:
-                destin_y = (int)pacman->y + 1;
+            case ACTION_DOWN:
+                destin_y = (int) pacman->y + 1;
+                dir = 3;
                 break;
-            case 4:
-                destin_x = (int)pacman->x - 1;
+            case ACTION_LEFT:
+                destin_x = (int) pacman->x - 1;
+                dir = 4;
                 break;
         }
         destin_x = destin_x < 0 ? destin_x % map->width + map->width : destin_x % map->width;
         destin_y = destin_y < 0 ? destin_y % map->height + map->height : destin_y % map->height;
-        if (map->cells[destin_x][destin_y] != CELL_BLOCK) {
-            //printf("\nhere\n");
-            //sleep(5);
-            return pacman->dir;
-        }
-        else return -1;
-    }*/
-    return 1;
+        if (map->cells[destin_x][destin_y] != CELL_BLOCK) return dir;
+    }
+
+    destin_y = pacman->y;
+    destin_x = pacman->x;
+    switch (pacman->dir){
+        case 1:
+            destin_y = (int)pacman->y - 1;
+            break;
+        case 2:
+            destin_x = (int)pacman->x + 1;
+            break;
+        case 3:
+            destin_y = (int)pacman->y + 1;
+            break;
+        case 4:
+            destin_x = (int)pacman->x - 1;
+            break;
+    }
+    destin_x = destin_x < 0 ? destin_x % map->width + map->width : destin_x % map->width;
+    destin_y = destin_y < 0 ? destin_y % map->height + map->height : destin_y % map->height;
+    if (map->cells[destin_x][destin_y] != CELL_BLOCK) return pacman->dir;
+    else return -1;
 }
