bcb5971b8a0a22e3aa2e62e7a6d8b8874a017af4
reza <ryarbakhsh@gmail.com>
2017-12-10 18:06:51 +0330

    deceide ghost debugged

--- src/physics.c
+++ src/physics.c
@@ -13,19 +13,21 @@ Direction decideGhost(const Map* map, Ghost* ghost) {
     int flag = 0;
     srand(clock());
     while (flag == 0){
+        destin_x = (int) ghost->x;
+        destin_y = (int) ghost->y;
         dir = rand() % 5;
         switch (dir) {
             case 1:
-                destin_y = (int)ghost->y - 1;
+                destin_y = (int)round(ghost->y - 1);
                 break;
             case 2:
-                destin_x = (int)ghost->x + 1;
+                destin_x = (int)round(ghost->x + 1);
                 break;
             case 3:
-                destin_y = (int)ghost->y + 1;
+                destin_y = (int)round(ghost->y + 1);
                 break;
             case 4:
-                destin_x = (int)ghost->x - 1;
+                destin_x = (int)round(ghost->x - 1);
                 break;
         }
         destin_x = destin_x < 0 ? destin_x % map->width + map->width : destin_x % map->width;
@@ -34,11 +36,10 @@ Direction decideGhost(const Map* map, Ghost* ghost) {
     }
     dir = dir == 0 ? -1 : dir;
     return dir;
-    //return 1;
 }
 
 Direction decidePacman(const Map* map, Pacman* pacman, Action action) {
-    int dir = pacman->dir;
+/*    int dir = pacman->dir;
     int destin_y = pacman->y, destin_x =pacman->x;
     switch (action){
         case ACTION_UP:
@@ -84,5 +85,6 @@ Direction decidePacman(const Map* map, Pacman* pacman, Action action) {
             return pacman->dir;
         }
         else return -1;
-    }
+    }*/
+    return 1;
 }
